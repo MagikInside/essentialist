9c0e5d1146abf7535c52d5231d3ba1b50bbecba3
lmoran <lmoran@gradiant.org>
2021-05-28 20:00:57 +0200

    add initial version of the basic components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NewTripComponent} from './new-trip/new-trip.component';
+import {MainComponent} from './main/main.component';
+import {TripDetailsComponent} from './trip-details/trip-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'new-trip', component: NewTripComponent},
+  { path: 'trip/:hash', component: TripDetailsComponent},
+  { path: '', component: MainComponent },
+  { path: '**', component: MainComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { NewTripComponent } from './new-trip/new-trip.component';
+import { TripDetailsComponent } from './trip-details/trip-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    NewTripComponent,
+    TripDetailsComponent
   ],
   imports: [
     BrowserModule,
