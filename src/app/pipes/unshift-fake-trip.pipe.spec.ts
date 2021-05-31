aaf215d0d932921da5a3cd793fcb8a1e62f32a03
lmoran <lmoran@gradiant.org>
2021-05-31 19:23:30 +0200

    test the unshift fake trip pipe

--- src/app/pipes/unshift-fake-trip.pipe.spec.ts
+++ src/app/pipes/unshift-fake-trip.pipe.spec.ts
@@ -1,8 +1,34 @@
 import { UnshiftFakeTripPipe } from './unshift-fake-trip.pipe';
+import {tripsStub} from '../stubs/trips-stub';
 
 describe('UnshiftFakeTripPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UnshiftFakeTripPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: UnshiftFakeTripPipe;
+
+  beforeEach(() => {
+    pipe = new UnshiftFakeTripPipe();
+  })
+
+  it('it should return a new array', () => {
+    const hash = 'New Trip';
+    const newTrips = pipe.transform(tripsStub, hash);
+    expect(newTrips).not.toBe(tripsStub);
   });
+  it('it should return the same array of trips with a additional trip at the beginning with the given string as hash', () => {
+    const hash = 'New Trip';
+    const newTrips = pipe.transform(tripsStub, hash);
+    newTrips!.forEach((trip, index) => {
+      if(index === 0) {
+        expect(trip.hash).toEqual(hash);
+      } else {
+        console.log(index, trip.hash, tripsStub[index - 1].hash);
+        expect(trip.hash).toEqual(tripsStub[index - 1].hash);
+      }
+    });
+  });
+  it('it should add a trip with the given hash in the case of empty trips array', () => {
+    const hash = 'New Trip';
+    const newTrips = pipe.transform([], hash);
+    expect(newTrips![0].hash).toEqual(hash);
+  });
+
 });
--- src/app/pipes/unshift-fake-trip.pipe.ts
+++ src/app/pipes/unshift-fake-trip.pipe.ts
@@ -8,12 +8,12 @@ export class UnshiftFakeTripPipe implements PipeTransform {
 
   transform(value: Trip[] | null, hash: string): Trip[] | null {
     if(value) {
-      value.unshift({
-        hash, image: '', visibility_status: '', arrival_date: '', departure_date: '',
+      return [{hash, image: '', visibility_status: '', arrival_date: '', departure_date: '',
         title: '', adults: 0, children: 0, infants: 0
-      });
+      }].concat(value);
+    } else {
+      return value;
     }
-    return value;
   }
 
 }
